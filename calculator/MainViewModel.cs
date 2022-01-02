eee66ff24e8c9148a72a3f5fe0d405d9f5a85cf4
en9er <frendjoy55@gmail.com>
2022-01-02 20:52:42 +0300

    Db and file history added

---
+++ calculator/MainViewModel.cs
@@ -0,0 +1,301 @@
+﻿#define DBHISTORY
+using System;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Input;
+using Newtonsoft.Json;
+
+namespace calculator
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        string path = $@"C:\Users\{Environment.UserName}\history.txt";
+        public ICommand IButtonClicked { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private expreval.Expreval calc = new expreval.Expreval();
+        private string _expression = "";
+        private string _hotExpression = "";
+        private int _historyVisibility = 0;
+        private ObservableCollection<string> _recentExpressions = new ObservableCollection<string>();
+        public ICommand SolveCommand { get; }
+        public bool Error
+        {
+            get
+            {
+                return calc.err;
+            }
+            set
+            {
+                if (value != calc.err)
+                    calc.err = value;
+            }
+
+        }
+        public string Expression
+        {
+            get
+            {
+                return _expression;
+            }
+            set
+            {
+                if (value == _expression)
+                    return;
+                _expression = value;
+                OnPropertyChanged(nameof(Expression));
+            }
+        }
+        public int HistoryVisibility
+        {
+            get
+            {
+                return _historyVisibility;
+            }
+            set
+            {
+                if (_historyVisibility != value)
+                    _historyVisibility = value;
+                OnPropertyChanged(nameof(HistoryVisibility));
+            }
+        }
+
+        public string HotExpression
+        {
+            get
+            {
+                return _hotExpression;
+            }
+            set
+            {
+                if (value != _hotExpression)
+                    _hotExpression = value;
+                OnPropertyChanged(nameof(HotExpression));
+            }
+        }
+
+        public void Solve(string exp)
+        {
+
+            string sourceExp = exp;
+            exp = calc.DeleteWhiteSpaces(exp);
+            exp = exp.Replace('×', '*');
+            exp = exp.Replace('÷', '/');
+            exp = calc.EvaluateExpression(exp);
+
+            if (Error)
+            {
+                Expression = "Error";
+            }
+            else
+            {
+                Expression = exp;
+                HotExpression = "";
+                if (_recentExpressions.Count == 0)
+                    _recentExpressions.Insert(0, sourceExp);
+                else if (sourceExp != _recentExpressions[0])
+                    _recentExpressions.Insert(0, sourceExp);
+
+#if DBHISTORY
+                HistoryWriteDB(sourceExp);
+#else
+                StreamWriter sw = System.IO.File.AppendText(path);
+                sw.WriteLine(sourceExp);
+                sw.Close();
+#endif
+            }
+        }   
+        public void HotSolve(string exp)
+        {
+            exp = calc.DeleteWhiteSpaces(exp);
+            exp = exp.Replace('×', '*');
+            exp = exp.Replace('÷', '/');
+            exp = calc.EvaluateExpression(exp);
+
+            if (Error)
+            {
+                HotExpression = "Error";
+            }
+            else
+            {
+                HotExpression = exp;
+            }
+        }
+
+        public ObservableCollection<string> RecentExpressions
+        {
+            get
+            {
+                return _recentExpressions;
+            }
+        }
+
+        public void LoadHistory(string path)
+        {
+            foreach (var str in System.IO.File.ReadAllLines(path))
+            {
+                _recentExpressions.Add(str);
+            }
+        }
+
+        public void HistoryReadDB()
+        {
+            using (var connection = new System.Data.SQLite.SQLiteConnection($"Data Source=C:\\Users\\{Environment.UserName}\\Downloads\\sqlitestudio-3.3.3\\SQLiteStudio\\history.db"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "select * from history";
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string exp = Convert.ToString(reader["value"]);
+                    _recentExpressions.Add(exp);
+                }
+
+            }
+        }
+
+        public void HistoryWriteDB(string exp)
+        {
+
+            using (var connection = new System.Data.SQLite.SQLiteConnection($"Data Source =C:\\Users\\{Environment.UserName}\\Downloads\\sqlitestudio-3.3.3\\SQLiteStudio\\history.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "Insert into history values " + "(\"" + exp + "\")"; 
+                command.ExecuteReader();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+#if DBHISTORY
+            HistoryReadDB();
+#else
+            if (System.IO.File.Exists(path))
+            {
+                LoadHistory(path);
+            }
+            else
+            {
+                System.IO.File.Create(path);
+            }
+#endif
+            IButtonClicked = new RelayCommand<string>(x =>
+            {
+                if (Error)
+                {
+                    if (x == "⌫")
+                    {
+                        if (Expression.Length != 0)
+                            Expression = Expression.Substring(0, Expression.Length - 1);
+                        Error = false;
+                    }
+                }
+                if (x == "=")
+                {
+                    Solve(Expression);
+                    HotExpression = "";
+                }
+                else if (x == "history")
+                {
+                    if (HistoryVisibility == 0)
+                        HistoryVisibility = 1000;
+                    else
+                        HistoryVisibility = 0;
+                }
+                else if (x == "clear_history")
+                {
+#if DBHISTORY
+                    using (var connection = new System.Data.SQLite.SQLiteConnection($"Data Source =C:\\Users\\{Environment.UserName}\\Downloads\\sqlitestudio-3.3.3\\SQLiteStudio\\history.db"))
+                    {
+                        connection.Open();
+                        var command = connection.CreateCommand();
+                        command.CommandText = "Delete from history";
+                        command.ExecuteReader();
+                        _recentExpressions.Clear();
+                    }
+#else
+                    _recentExpressions.Clear();
+                    System.IO.File.Delete(path);
+                    System.IO.File.Create(path);
+#endif
+                }
+                else if (x == "C")
+                {
+                    Expression = "";
+                    Error = false;
+                }
+                else if (x == "⌫")
+                {
+                    if (Expression.Length != 0)
+                        Expression = Expression.Substring(0, Expression.Length - 1);
+                }
+                else if (x == "Enter")
+                {
+                    Solve(Expression);
+                }
+                else
+                {
+                    if (Expression == "0" && x == "0")
+                    {
+
+                    }
+                    else if (Expression.Contains(",") && x == ",")
+                    {
+                        string str = Expression;
+                        int index = str.Length - 1;
+                        while (char.IsDigit(str[index]))
+                        {
+                            index--;
+                        }
+                        if (str[index] != ',')
+                            Expression += x;
+                    }
+                    else if ((x == "+" || x == "-" || x == "×" || x == "÷") && Expression.Length != 0)
+                    {
+                        char c = Expression[Expression.Length - 1];
+                        if (c == x[0])
+                        {
+
+                        }
+                        else if (c == '+' || c == '-' || c == '×' || c == '÷')
+                        {
+                            Expression = Expression.Substring(0, Expression.Length - 1) + x;
+                        }
+                        else if (c == '(')
+                        {
+
+                        }
+                        else
+                        {
+                            Expression += x;
+                        }
+
+                    }
+                    else
+                    {
+                        Expression += x;
+                    }
+
+
+                }
+                //Hot Solve Logic
+                if (x != "+" && x != "-" && x != "÷" && x != "×" && x != "=" && x != "Enter")
+                {
+                    HotSolve(Expression);
+                }
+            }, x => string.IsNullOrWhiteSpace(x) == false);
+        }
+
+    }
+}
---
+++ str_solve/expreval.cs
@@ -0,0 +1,365 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using FluentAssertions;
+
+namespace expreval
+{
+    public class Expreval
+    {
+        public bool err = false;
+        public bool lackBrace = false;
+        public string ReplaceSeparators(string expr)
+        {
+            int i = 0;
+            string newStr = "";
+            while (i < expr.Length)
+            {
+                if (expr[i] == ',')
+                {
+                    newStr += '.';
+                }
+                else
+                {
+                    newStr += expr[i];
+                }
+                i++;
+            }
+            return newStr;
+        }
+        public string ReverseString(string str)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return new String(arr);
+        }
+
+
+        public int IndexOfCorrespondingRightBrace(int leftBraceIndex, string str)
+        {
+            if (str[leftBraceIndex] != '(')
+            {
+                Console.WriteLine("The symbol with received index is not brace");
+                return -1;
+            }
+
+            int innerBraces = 0;
+            for (int i = leftBraceIndex + 1; i < str.Length; i++)
+            {
+                if (str[i] == '(')
+                {
+                    innerBraces++;
+                }
+                else if (str[i] == ')')
+                {
+                    innerBraces--;
+                }
+                if (innerBraces < 0)
+                    return i;
+            }
+            return -1;
+
+
+        }
+
+
+        public double ParseArgument(string arg, bool right = false)
+        {
+            if (!right)
+                arg = ReverseString(arg);
+            double result;
+
+            //current culture
+            if (!double.TryParse(arg, System.Globalization.NumberStyles.Any, CultureInfo.CurrentCulture, out result) &&
+                //US english
+                !double.TryParse(arg, System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out result) &&
+                //neutral language
+                !double.TryParse(arg, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+            {
+                return 0;
+            }
+
+            return result;
+        }
+
+        public double EvaluateExpressionWithNoBraces(string exp)
+        {
+            Stack<char> operators = new Stack<char>(new char[] { '-', '+', '/', '*' });
+            while (operators.Count > 0)
+            {
+                if ((exp.IndexOf('*') == -1) && (exp.IndexOf('/') == -1) && (exp.IndexOf('+') == -1) && exp.StartsWith("-"))
+                {
+                    int minusCount = 0;
+                    foreach (char c in exp)
+                    {
+                        if (c == '-')
+                        {
+                            minusCount++;
+                        }
+                    }
+
+                    if (minusCount < 2)
+                    {
+                        break;
+                    }
+                }
+                string right = "";
+                string left = "";
+                int index;
+                if (exp[0] == '-' && operators.Peek() == '-')
+                {
+                    index = exp.Substring(1).IndexOf(operators.Peek());
+                    index++;
+                }
+                else
+                {
+                    index = exp.IndexOf(operators.Peek());
+                }
+                if (index != -1)
+                {
+                    for (int j = index + 1; j < exp.Length; j++)
+                    {
+                        if (right.Length == 0 && exp[j] == '-')
+                            right += exp[j];
+                        else if (('0' <= exp[j] && exp[j] <= '9'))
+                        {
+                            right += exp[j];
+                        }
+                        else if (exp[j] == ',' || exp[j] == '.')
+                        {
+                            right += exp[j];
+                        }
+                        else
+                            break;
+                    }
+
+                    for (int j = index - 1; j >= 0; j--)
+                    {
+                        if (('0' <= exp[j] && exp[j] <= '9') || (exp[j] == '.' || exp[j] == ',') || (exp[j] == '-'))
+                        {
+                            left += exp[j];
+                            if (exp[j] == '-')
+                                break;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    double dleft = ParseArgument(left);
+                    double dright = ParseArgument(right, true);
+
+
+                    double res = 0;
+                    switch (operators.Peek())
+                    {
+                        case '*':
+                            {
+                                res = dleft * dright;
+                                break;
+                            }
+                        case '/':
+                            {
+                                try
+                                {
+                                    res = dleft / dright;
+                                }
+                                catch (DivideByZeroException)
+                                {
+                                    throw new DivideByZeroException("Division by zero!");
+                                }
+                                break;
+                            }
+                        case '+':
+                            {
+                                res = dleft + dright;
+                                break;
+                            }
+                        case '-':
+                            {
+                                res = dleft - dright;
+                                break;
+                            }
+                    }
+                    string s = exp.Substring(index - left.Length, left.Length + 1 + right.Length);
+                    if (s[0] == '-')
+                        exp = exp.Replace(exp.Substring(index - left.Length, left.Length + 1 + right.Length), ("+" + res.ToString()));
+                    else
+                        exp = exp.Replace(exp.Substring(index - left.Length, left.Length + 1 + right.Length), res.ToString());
+                }
+                else
+                {
+                    operators.Pop();
+                }
+            }
+            double r;
+            if (double.TryParse(exp, out r))
+            {
+                return r;
+            }
+            {
+                err = true;
+                return 0;
+            }
+
+        }
+
+
+
+        public string EvaluateExpression(string exp)
+        {
+            if (exp.Contains("+") || exp.Contains("-") || exp.Contains("*") || exp.Contains("/") || exp.Contains("(") || exp.Contains(")"))
+            {
+                if (exp.Contains("("))
+                {
+                    if (exp.Contains(")"))
+                    {
+                        int ind = IndexOfCorrespondingRightBrace(exp.IndexOf('('), exp);
+                        if (ind == -1)
+                        {
+                            err = true;
+                            return "Error";
+                        }
+                        string subExpr = exp.Substring(exp.IndexOf('('), ind - exp.IndexOf('(') + 1);
+                        string newExpr = EvaluateExpression(subExpr.Substring(1, subExpr.Length - 2));
+                        int i = exp.IndexOf(subExpr);
+                        if (i != 0)
+                        {
+                            if (char.IsDigit(exp[i - 1]))
+                            {
+                                newExpr = "*" + newExpr;
+                            }
+                        }
+                        exp = exp.Replace(subExpr, newExpr);
+                        return EvaluateExpression(exp);
+                    }
+                    else
+                    {
+                        lackBrace = true;
+                        return "close )";
+                        //throw new SystemException(") expected");
+                    }
+                }
+                else if (exp.Contains(")"))
+                {
+                    lackBrace = true;
+                    return "open (";
+                    //throw new SystemException("Error ( expected");
+                }
+                else
+                {
+                    return EvaluateExpressionWithNoBraces(exp).ToString();
+                }
+            }
+            else
+            {
+                return exp;
+            }
+        }
+
+        public string DeleteWhiteSpaces(string expression)
+        {
+            string res = "";
+            foreach (char sym in expression)
+            {
+                if (sym != ' ')
+                    res += sym;
+            }
+            return res;
+        }
+    }
+
+
+    [TestFixture]
+    class ExprevalTests
+    {
+        Expreval _expreval;
+
+        [SetUp]
+        public void Setup()
+        {
+            _expreval = new Expreval();
+        }
+
+        [Test]
+        public void ShouldReturnCorrespondingBraceIndex()
+        {
+            string expression = "(2+(3+5)+4)";
+            Expreval tmp = new Expreval();
+            expression = tmp.DeleteWhiteSpaces(expression);
+            var res = tmp.IndexOfCorrespondingRightBrace(0, expression);
+
+            res.Should().Be(10);
+        }
+
+        [TestCase("2 + 4 + ( 4 - 1 ) ", "2+4+(4-1)")]
+        public void ShouldDeleteWhiteSpaces(string expression, string result)
+        {
+            expression = _expreval.DeleteWhiteSpaces(expression);
+            expression.Should().Be(result);
+        }
+
+
+
+        [TestCase("-2+4", 2)]
+        [TestCase("8.2 + 4 - 3", 9.2)]
+        [TestCase("12 + 6", 18)]
+        [TestCase("3", 3)]
+        [TestCase("3 + 4", 7)]
+        [TestCase("3 + 4 * 3.1", 15.4)]
+        [TestCase("3 / 4 * 1", 0.75)]
+        [TestCase("0 * 3 - 1", -1)]
+        [TestCase("666 * 666", 443556)]
+        [TestCase("-1", -1)]
+        [TestCase("+1", 1)]
+        [TestCase("5", 5)]
+        [TestCase("1-3--5", 3)]
+        [TestCase("1000.5-7-7-7-7-7-7-7-7-7-7-7-7-7-7-7-7-7-7-7-7", 860.5)]
+        [TestCase("1000--7-7-7-7-7-7-7--7--7-7-7----------------------7-7--7---7--7-7-7-7-7---7", 937)]
+        public void ShouldSolveExpressionWithNoBraces(string testString, double expected)
+        {
+            Expreval tmp = new Expreval();
+            testString = tmp.DeleteWhiteSpaces(testString);
+            var res = tmp.EvaluateExpressionWithNoBraces(testString);
+            res.Should().Be(expected);
+        }
+
+
+        [TestCase("1 + 123 - (4 * 5)", "104")]
+        [TestCase("5 + (2 - 4)", "3")]
+        [TestCase("5 + 9", "14")]
+        [TestCase("(3 + 4)", "7")]
+        [TestCase("13 + 4 - 4 / (3 + 1)", "16")]
+        [TestCase("1 + ((1 + 1) + 1) + (1 + 1) + 1", "7")]
+        [TestCase("1000 - 7 * 3 - 1 - 1 - 8 - 343 - (2 - 1 * (5 * 4))", "644")]
+        [TestCase("1000 - 7 - 7 - 7- 7- 7- 7- 7- 7- 7- 7- 7- 7- 7- 7- 7- 7- 7- 7- 7- 7- 7- 7- 7 -7- 7- 7- 7- 7- 7 -7", "790")]
+        [TestCase("((2+3) * 4)", "20")]
+        [TestCase("50-25+5000000000000000000000", "5,5E+22")]
+        //[TestCase("55/555555555555555", "9,9×E−14")]
+        [TestCase("(((10 / 5)+3) * 4)", "20")]
+        [TestCase("", "")]
+        public void ShouldReturnSolvedExpression(string testString, string expected)
+        {
+            Expreval tmp = new Expreval();
+            testString = tmp.DeleteWhiteSpaces(testString);
+            string res = tmp.EvaluateExpression(testString);
+            res.Should().Be(expected);
+        }
+
+
+        [Test]
+        public void ShouldReturnStrWithCorrectSeparators()
+        {
+            Expreval tmp = new Expreval();
+            string exp = "2,3 + 4,2";
+            string res = "2.3+4.2";
+            exp = tmp.DeleteWhiteSpaces(exp);
+            exp = tmp.ReplaceSeparators(exp);
+            res.Should().Be(exp);
+        }
+    }
+
+}
